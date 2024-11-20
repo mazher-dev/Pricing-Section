40e65c9850fb383494b0b34ef7b8b3a3b5936dfe
MH Code <mazheriftikhar1@gmail.com>
2024-11-20 22:07:18 +0500

    20-Nov changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import Pricing from './Components/Pricing/Pricing'
+import Pricing2 from './Components/Pricing2/Pricing2'
+import Pricing3 from './Components/Pricing3/Pricing3'
+import Pricing4 from './Components/Pricing4/Pricing4'
+import Pricing5 from './Components/Pricing5.jsx/Pricing5'
+import Pricing6 from './Components/Pricing6/Pricing6'
+import Pricing8 from './Pricing8/Pricing8'
 
 const App = () => {
   return (
     <div>
-      Hello world
+      <Pricing />
+      <Pricing2 />
+      <Pricing3 />
+      <Pricing4 />
+      <Pricing5 />
+      <Pricing6 />
+      <Pricing8 />
     </div>
   )
 }
